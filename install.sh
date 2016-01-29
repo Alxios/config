398680328166e8b0a9d7e34625126a940f894ea9
Alexandre GASPAR <agaspar@student.42.fr>
2016-01-29 18:00:56 +0100

    Config

---
+++ install.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# Default install
+DEFAULT=true
+# LN contain files which will get a symlink
+LN=("aliases" "ls_colors" "vim" "vimrc" "zshrc")
+# DEST is the destination dir
+DEST=$HOME
+# SRC is the source dir
+SRC="${DEST}/.dotfiles"
+
+# display usage of the script
+function usage()
+{
+    echo -e "Usage: $1 <options>\n"
+    echo "By default the following symlink will be created :"
+    for i in ${LN[@]}; do
+        echo -e "\t${DEST}/.${i} -> ${SRC}/${i}"
+    done
+    echo ""
+    echo "Suported options :"
+    echo -e "\t-a|--available-files : list all available file on which you can crete symlink"
+    echo -e "\t-c|--clean : delete all symlink and restore previous file if any"
+    echo -e "\t-e|--existing : list all existing symlink"
+    echo -e "\t-f|--files file1,file2 : give a list a files comma separated"
+    echo -e "\t-h|--help : display this help"
+    echo -e "\t-l|--symlink: update symlink"
+    echo -e "\t-s|--submodules : init and update submodules"
+    echo -e "\t-g|--geam : my personnal config /!\\ DO NOT USE IT"
+    echo -e "\t--g42 : my personnal config for 42 /!\\ DO NOT USE IT"
+    exit 0
+}
+
+# create the symlink
+function do_ln()
+{
+    for i in ${LN[@]}; do
+        if [[ -f "${SRC}/${i}" ]] || [[ -d "${SRC}/${i}" ]]; then
+            if [[ -f "${DEST}/.${i}" ]] || [[ -d "${DEST}/.${i}" ]]; then
+                mv "${DEST}/.${i}" "${DEST}/.${i}.old"
+            fi
+            ln -s "${SRC}/${i}" "${DEST}/.${i}"
+        fi
+    done
+}
+
+# delete the symlink
+function rm_ln()
+{
+    FILES=$(ls ${SRC})
+    for i in ${FILES[@]}; do
+        if [[ -L "${DEST}/.${i}" ]]; then
+            rm "${DEST}/.${i}"
+            if [[ -f "${DEST}/.${i}.old" ]] || [[ -d "${DEST}/.${i}.old" ]]; then
+                mv "${DEST}/.${i}.old" "${DEST}/.${i}"
+            fi
+        fi
+    done
+}
+
+# list the existing symlink
+function existing()
+{
+    FILES=$(ls ${SRC})
+    for i in ${FILES[@]}; do
+        if [[ -L "${DEST}/.${i}" ]]; then
+            ls -l "${DEST}/.${i}"
+        fi
+    done
+}
+
+# redifine LN variable based on user input
+function get_files_list()
+{
+    IFS=','
+    LN=($1)
+}
+
+# get the arg of the script
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            usage $0
+            ;;
+        -c|--clean)
+            DELETE=true
+            unset DEFAULT
+            ;;
+        -a|--available_files)
+            ls -l ${SRC}
+            exit 0
+            ;;
+        -e|--existing)
+            existing
+            exit 0
+            ;;
+        -f|--files)
+            shift
+            get_files_list $1
+            ;;
+        -s|--submodules)
+            if [[ -n "${DEFAULT}" ]]; then
+                unset DEFAULT
+            fi
+            SUBMODULES=true
+            ;;
+        -l|--symlink)
+            if [[ -n "${DEFAULT}" ]]; then
+                unset DEFAULT
+            fi
+            SYMLINK=true
+            ;;
+        -g|--geam)
+            GEAM=true
+            LINUX=true
+            ;;
+        --g42)
+            GEAM=true
+            G42=true
+            ;;
+        *)
+            usage $0
+            ;;
+    esac
+    shift
+done
+
+if [[ -n "${DELETE}" ]] || [[ -n "${SYMLINK}" ]]; then
+    rm_ln
+fi
+
+if [[ -n "${DEFAULT}" ]] || [[ -n "${SYMLINK}" ]]; then
+    # if it's me, add the git config
+    LN+=("gitconfig" "gitignore_global")
+
+    # if it's me on linux
+    if [[ -n "${LINUX}" ]]; then
+        LN+=("irssi" "tmux" "tmux.config")
+    fi
+
+    do_ln
+fi
+
+if [[ -n "${G42}" ]]; then
+    cd $HOME
+    if [[ ! -d "$HOME/libft" ]]; then
+        git clone git@github.com:Geam/libft.git libft
+    fi
+    if [[ ! -d "$HOME/scripts" ]]; then
+        git clone git@github.com:Geam/scripts.git scripts
+    fi
+    if [[ ! -L "$HOME/sgoinfre" ]]; then
+        ln -s /nfs/sgoinfre
+    fi
+    brew update
+    source $HOME/.zshrc
+    brew install htop tig
+fi
---
+++ zshrc
@@ -0,0 +1,146 @@
+# Definition du PATH
+PATH=$HOME/scripts:$HOME/.brew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin
+export PATH
+
+# Configuration de l'historique
+HISTFILE=~/.zshrc_history
+SAVEHIST=5000
+HISTSIZE=5000
+setopt inc_append_history
+setopt share_history
+
+# Tmux command history
+bindkey '^R' history-incremental-search-backward
+bindkey -e
+export LC_ALL=en_US.UTF-8
+
+# search in history based on what is type
+bindkey '\e[A' history-beginning-search-backward
+bindkey '\e[B' history-beginning-search-forward
+
+# previous/next word with ctrl + arrow
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' backward-word
+
+# default editor
+EDITOR=/usr/bin/vim
+export EDITOR
+
+# Reglage du terminal
+if [ "$SHLVL" -eq 1 ]; then
+    TERM=xterm-256color
+fi
+
+# Correction de la touche Delete
+bindkey "\e[3~"   delete-char
+
+# add completion provied by bin installed via brew
+if [[ -d "$HOME/.brew/share/zsh/site-functions/" ]]; then
+    fpath=($HOME/.brew/share/zsh/site-functions/ $fpath)
+fi
+
+# Autocompletion amelioree
+autoload -U compinit && compinit
+
+# Autocompletion de type menu
+zstyle ':completion:*' menu select
+
+# Couleur prompt
+autoload -U colors && colors
+
+# fucking mac and their /Volume/<hdd_name>
+cd "`echo $PWD | sed 's:/Volumes/Data::'`"
+
+# Definition des variables
+USER=`/usr/bin/whoami`
+export USER
+GROUP=`/usr/bin/id -gn $user`
+export GROUP
+MAIL="$USER@student.42.fr"
+export MAIL
+LIB="$HOME/libft"
+export LIB
+
+# Definition des couleurs
+if [ -f ~/.ls_colors ]; then
+    source ~/.ls_colors
+fi
+
+NORMAL="%{$reset_color%}"
+
+# Definition du prompt
+precmd ()
+{
+    if [ $? -eq 0 ]
+    then
+        COLOR3="%{$fg[green]%}"
+    else
+        COLOR3="%{$fg[red]%}"
+    fi
+    ISGIT=$(git status 2> /dev/null)
+    if [ -n "$ISGIT" ]
+    then
+        STATUS=$(echo "$ISGIT" | grep "modified:\|renamed:\|new file:\|deleted:" | grep -v ".vim/bundle\|untracked")
+        BRANCH=$(git branch | cut -d ' ' -f 2 | tr -d '\n')
+        if [ -n "$STATUS" ]
+        then
+            COLOR="%{$fg[red]%}"
+        else
+            REMOTE_EXIST=$(git branch -a | grep remotes/origin/$BRANCH)
+            if [ -n "$REMOTE_EXIST" ]
+            then
+                REMOTE=$(git diff origin/$BRANCH)
+                if [ -n "$REMOTE" ]
+                then
+                    COLOR="%{$fg[yellow]%}"
+                else
+                    COLOR="%{$fg[green]%}"
+                fi
+            else
+                COLOR="%{$fg[green]%}"
+            fi
+        fi
+        RPROMPT="%{$COLOR%}($BRANCH)%{$NORMAL%}"
+    else
+        RPROMPT=""
+    fi
+    PROMPT="%B%{$fg[green]%}%n@%m%{$NORMAL%}%B:%{$fg[blue]%}%~%{$NORMAL%}
+%B%{$COLOR3%}> %{$NORMAL%}%b"
+}
+
+# Load global aliases
+if [ -f ~/.aliases ]; then
+    source ~/.aliases
+fi
+
+# Couleurs pour le man
+man()
+{
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+        man "$@"
+}
+
+# update symlink in case of zsf change
+if [[ ! -f $HOME/.old_home ]]; then
+    echo $HOME > $HOME/.old_home
+fi
+OLD_HOME=$(cat $HOME/.old_home)
+if [[ "$OLD_HOME" != "$HOME" ]]; then
+    echo $HOME > $HOME/.old_home
+    if [[ `basename $HOME` = "mdelage" ]]; then
+        env GEAM=true $HOME/.dotfiles/install.sh -c -l
+    else
+        $HOME/.dotfiles/install.sh -c -l
+    fi
+    echo "+------------------------+"
+    echo "| /!\\ You've changed zsf |"
+    echo "+------------------------+"
+fi
+source /nfs/zfs-student-2/users/agaspar/.Sublivim/sublivimrc.sh
